6483f7c5a22b26c86c574fba5d6e0200061baf9b
veerasamy <veera.bca09@gmail.com>
2025-07-15 19:13:09 +0530

    implement CRUD routes in app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.get("/User",(req,res)=>{
+    res.send({"firstname":'veera',LastName:"samy"})
+})
+
+app.post("/User",(req,res)=>{
+    res.send("Data saved sucessfully")
+})
+app.put("/User",(req,res)=>{
+    res.send("Data modified sucessfully")
+})
+app.delete("/User",(req,res)=>{
+    res.send("Data deleted sucessfully")
+})
+app.use("/Test",(req,res)=>{
+    res.send("hello from server")
+})
+
 app.listen(3000, () => {
     console.log("server is running on port 3000");
 });
